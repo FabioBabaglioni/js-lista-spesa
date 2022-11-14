86b4be679eab0ec0210c2352ae2675948e8ecf12
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-14 15:08:56 +0100

    esercizio con for e while solo console.log

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+// data una lista della spesa nota stampa i prodotti 
+
+let listaSpesa = ["Patate", "Carote", "Latte", "spinaci", "bagno schiuma"];
+
+// ciclo for per stampare i prodotti singoli
+// for(let i = 0; i < listaSpesa.length; i++){
+
+//     console.log(listaSpesa[i]);
+// };
+
+// ciclo while per stampare i prodotti singoli
+
+let i = 0
+
+while(i < listaSpesa.length){
+
+    console.log(listaSpesa[i])
+
+    i++
+}
